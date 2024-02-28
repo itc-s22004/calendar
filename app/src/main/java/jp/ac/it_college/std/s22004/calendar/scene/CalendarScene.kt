da27c7190ed995dee22c2aa6469caae45bf057d9
s22004 <s22004@std.it-college.ac.jp>
2024-02-29 02:07:47 +0900

    カレンダーのベース

--- app/src/main/java/jp/ac/it_college/std/s22004/calendar/scene/CalendarScene.kt
+++ app/src/main/java/jp/ac/it_college/std/s22004/calendar/scene/CalendarScene.kt
@@ -1,5 +1,7 @@
 package jp.ac.it_college.std.s22004.calendar.scene
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -18,11 +21,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kizitonwose.calendar.compose.HorizontalCalendar
 import com.kizitonwose.calendar.compose.rememberCalendarState
 import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.DayPosition
 import com.kizitonwose.calendar.core.OutDateStyle
 import com.kizitonwose.calendar.core.daysOfWeek
 import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
@@ -54,9 +64,6 @@ fun CustomDatePicker() {
         outDateStyle = OutDateStyle.EndOfGrid
     )
 
-    // 横スクロールのカレンダーを作成するためのComposable関数
-    // 縦スクロールのVerticalなどもある
-//    Text(text = "asdf")
     Column {
 
         HorizontalCalendar(
@@ -64,7 +71,26 @@ fun CustomDatePicker() {
             // 日付を表示する部分
             dayContent = {Day(it)},
             // カレンダーのヘッダー
-            monthHeader = {DaysOfWeekTitle(daysOfWeek = daysOfWeek)}
+//            monthHeader = {DaysOfWeekTitle(daysOfWeek = daysOfWeek)}
+            monthHeader = { month ->
+                val daysOfWeek = month.weekDays.first().map { it.date.dayOfWeek }
+                MonthHeader(daysOfWeek = daysOfWeek)
+            },
+            monthBody = { _, content ->
+                Box(
+                    modifier = Modifier.background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                MaterialTheme.colorScheme.primary.copy(alpha = 0.2f),
+                                MaterialTheme.colorScheme.tertiary.copy(alpha = 0.2f)
+                            )
+                        )
+                    )
+                        .border(width = 0.5.dp, color = Color.LightGray) // 追加
+                ) {
+                    content()
+                }
+            },
         )
     }
 }
@@ -86,15 +112,55 @@ fun DaysOfWeekTitle(daysOfWeek: List<DayOfWeek>) {
     }
 }
 
-
 @Composable
 private fun Day(day: CalendarDay) {
+    val textColor = when (day.position) {
+        DayPosition.MonthDate -> when (day.date.dayOfWeek) {
+            DayOfWeek.SATURDAY -> Color.Blue
+            DayOfWeek.SUNDAY -> Color.Red
+            else -> Color.Unspecified
+        }
+
+        DayPosition.InDate, DayPosition.OutDate -> Color.LightGray
+    }
     Box(
         modifier = Modifier
-            .aspectRatio(1f),
+            .aspectRatio(1f)
+            .border(width = 0.5.dp, color = Color.LightGray)
+            .padding(1.dp),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = day.date.dayOfMonth.toString())
+        Text(
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(top = 3.dp, start = 4.dp),
+            text = day.date.dayOfMonth.toString(), color = textColor
+        )
+    }
+}
+
+fun DayOfWeek.displayText(uppercase: Boolean = false): String {
+    return getDisplayName(TextStyle.SHORT, Locale.getDefault()).let { value ->
+        if (uppercase) value.uppercase(Locale.getDefault()) else value
+    }
+}
+
+@Composable
+private fun MonthHeader(daysOfWeek: List<DayOfWeek>) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .testTag("MonthHeader"),
+    ) {
+        for (dayOfWeek in daysOfWeek) {
+            Text(
+                modifier = Modifier.weight(1f),
+                textAlign = TextAlign.Center,
+                fontSize = 15.sp,
+                text = dayOfWeek.displayText(),
+                fontWeight = FontWeight.Medium,
+            )
+        }
     }
 }
 
